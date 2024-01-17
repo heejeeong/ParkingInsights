2337802bc0a3d1b2f364703a10af68fe8999355d
heejeeong <hdoubleez@khu.ac.kr>
2024-01-17 22:35:12 +0900

    Add source

--- source/createMap.py
+++ source/createMap.py
@@ -2,7 +2,7 @@ import folium
 import pandas as pd
 
 # 엑셀 파일에서 데이터 로드 (위도와 경도가 있는 열이 있다고 가정)
-data = pd.read_excel("C:/Users/heeje/OneDrive/바탕 화면/project/20221101_불법주정차.xlsx")
+data = pd.read_excel("../data/20221101_불법주정차.xlsx")
 
 # 지도 객체 생성
 map_object = folium.Map(location=[data['위도'].mean(), data['경도'].mean()], zoom_start=10)
@@ -12,5 +12,5 @@ for index, row in data.iterrows():
     folium.Marker(location=[row['위도'], row['경도']], popup=row['시간']).add_to(map_object)
 
 # 지도를 HTML 파일로 저장
-map_object.save('C:/Users/heeje/OneDrive/바탕 화면/project/map.html')
+map_object.save('../result/map1.html')
 print("끝났습니다.")
